486f9e73f78168211aca1fceb3e08ee4f5509965
paletva1 <vamshee_krishna.paleti@novartis.com>
2018-08-05 19:12:22 +0530

    added alert activity.

--- app/src/main/java/peelabus/com/Alert/AlertActivity.java
+++ app/src/main/java/peelabus/com/Alert/AlertActivity.java
@@ -3,22 +3,23 @@ package peelabus.com.Alert;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import peelabus.com.R;
 import peelabus.com.baseclasses.BaseActivity;
+import peelabus.com.home.OnHomeInteractionListener;
 
-public class AlertActivity extends BaseActivity {
+public class AlertActivity extends BaseActivity implements OnAlertInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_alert);
-        /*Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.alerts);
-        setSupportActionBar(toolbar)*/;
+        setContentView(R.layout.activity_root_view);
+        addFragment(AlertsFragment.newInstance("", ""), false, false, AlertsFragment.class.getName());
     }
 
     @Override
